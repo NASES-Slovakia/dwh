bdbefcca44f92ee641e266a7e73866de53be39e1
branislavchromiak <branislav.chromiak@rsgroup.com>
2026-09-15 10:42:40 +0200

    cleanup initial repository

--- wh_gold.Warehouse/base/Tables/metadata_table_column_setup_cache.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [base].[metadata_table_column_setup_cache] (
-
-	[gold_table_name] varchar(128) NULL, 
-	[silver_column_name] varchar(128) NULL, 
-	[gold_column_name] varchar(128) NULL, 
-	[ordinal_position] int NULL
-);
--- wh_gold.Warehouse/dbo/Views/column_info.sql
+++
@@ -1,5 +0,0 @@
--- Auto Generated (Do not modify) 57648963141C093A5A9FDF694FF980531CF37E547B99528504D29D477C4D6D3E
-create   view column_info as 
-    SELECT * 
-    FROM [lh_gold].[INFORMATION_SCHEMA].[COLUMNS]
-    WHERE TABLE_SCHEMA = 'base' -- Adjusted schema to 'base' as per schema listing
